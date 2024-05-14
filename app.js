7cc6b3a8b4298b7beed097986001dc2fe8d03829
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-14 13:43:14 +0200

    added template as component

--- app.js
+++ app.js
@@ -20,7 +20,38 @@ const app = Vue.createApp({
   },
   methods: {
     toogleDetails() {
-        this.detailsAreVisible = !this.detailsAreVisible;
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toogleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
 });
